b52ce5624248ca9a3f9cdb9aebee80db740d1344
Ken Ng <ken@sterlingpj.com>
2017-09-09 23:27:25 +0800

    added submit policy function

--- policies-api.js
+++ policies-api.js
@@ -16,7 +16,15 @@ app.get('/policies/:userId', function(req, res) {
     var payload = new Object();
     payload = [];
     for (var i=0; i<apiResponse.length; i++ ) {
-      payload.push({"policyNo":apiResponse[i].policyid,"price":apiResponse[i].price});
+      var policy = {
+        "country" : apiResponse[i].country,
+        "endDate" : apiResponse[i].endDate,
+        "policyNo" : apiResponse[i].policyId,
+        "price" : apiResponse[i].price,
+        "startDate" : apiResponse[i].startDate,
+        "userId" : apiResponse[i].userId
+      };
+      payload.push(policy);
     }
     console.log("received: " + JSON.stringify(apiResponse));
     console.log("payload: " + JSON.stringify(payload));
@@ -24,6 +32,37 @@ app.get('/policies/:userId', function(req, res) {
     res.send(payload);
   })
 });
+
+app.post('/policies', function(req, res) {
+  var payload = req.body;
+  var policyNo = generatePolicyNumber();
+  var policy = {
+    "name" : payload.userId + " - " + policyNo,
+    "country" : payload.country,
+    "endDate" : payload.endDate,
+    "policyId" : policyNo,
+    "price" : payload.price,
+    "startDate" : payload.startDate,
+    "userId" : payload.userId
+  };
+  console.log("policy: " + JSON.stringify(policy));
+
+  submitPolicy(policy, (apiResponse) => {
+    var resPayload = {
+      "country" : apiResponse.entities[0].country,
+      "endDate" : apiResponse.entities[0].endDate,
+      "policyNo" : apiResponse.entities[0].policyId,
+      "price" : apiResponse.entities[0].price,
+      "startDate" : apiResponse.entities[0].startDate,
+      "userId" : apiResponse.entities[0].userId
+    };
+    console.log("received: " + JSON.stringify(apiResponse));
+    console.log("resPayload: " + JSON.stringify(payload));
+
+    res.send(payload);
+  })
+});
+
 
 // Listen for requests until the server is stopped
 app.listen(process.env.PORT || 9000);
@@ -50,4 +89,36 @@ function getPolicies(userId, callback) {
         });
     });
     req.end();
+}
+
+function submitPolicy(policy, callback) {
+  var options = {
+    host: 'apibaas-trial.apigee.net',
+    port: 443,
+    path: '/kentrial/sandbox/policies',
+    method: 'POST'
+  };
+  var req = https.request(options, res => {
+        res.setEncoding('utf8');
+        var returnData = "";
+        res.on('data', chunk => {
+            returnData = returnData + chunk;
+        });
+        res.on('end', () => {
+            var pop = JSON.parse(returnData);
+            callback(pop);
+        });
+    });
+    req.write(JSON.stringify(policy));
+    req.end();
+}
+
+function generatePolicyNumber() {
+    var text = "";
+    var length = 8;
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for(var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+  return "P-" + text;
 }
